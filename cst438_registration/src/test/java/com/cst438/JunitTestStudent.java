79facafca05ec7dd8bdc60b148e45435a92200b4
OsCortes88 <ocortestinoco@csumb.edu>
2023-09-26 16:11:17 -0700

    Update APIs to use StudentDTO

--- cst438_registration/src/test/java/com/cst438/JunitTestStudent.java
+++ cst438_registration/src/test/java/com/cst438/JunitTestStudent.java
@@ -2,6 +2,7 @@ package com.cst438;
 
 import com.cst438.domain.ScheduleDTO;
 import com.cst438.domain.Student;
+import com.cst438.domain.StudentDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.cst438.TestUtils.asJsonString;
 import static com.cst438.TestUtils.fromJsonString;
@@ -29,15 +33,12 @@ public class JunitTestStudent {
     public void updateStudent() throws Exception {
         MockHttpServletResponse response;
 
-        Student updateStudent = new Student();
-        updateStudent.setStudent_id(3);
-        updateStudent.setName("Ryan");
-        updateStudent.setEmail("ryan@csumb.edu");
+        StudentDTO updateStudent = new StudentDTO(3,"Ryan", "ryan@csumb.edu", 0, null);
 
         // Perform update
         response = mvc.perform(
                 MockMvcRequestBuilders
-                        .put("/student/update")
+                        .put("/student/3")
                         .content(asJsonString(updateStudent))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
@@ -46,21 +47,19 @@ public class JunitTestStudent {
         // Verify update
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .get("/student/list")
+                                .get("/student")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        Student[] student_list = fromJsonString(response.getContentAsString(), Student[].class);
+        StudentDTO[] student_list = fromJsonString(response.getContentAsString(), StudentDTO[].class);
         boolean found = false;
-        for (Student s : student_list) {
-            if (s.getName().equals(updateStudent.getName()) && s.getEmail().equals(updateStudent.getEmail())) {
+        for (StudentDTO s : student_list) {
+            if (s.name().equals(updateStudent.name()) && s.email().equals(updateStudent.email())) {
                 found = true;
             }
         }
         assertTrue(found);
-
-
     }
 
     @Test
@@ -75,31 +74,30 @@ public class JunitTestStudent {
         // Simulates a successful response for adding a student
         response = mvc.perform(
                 MockMvcRequestBuilders
-                        .post("/student/add")
+                        .post("/student")
                         .content(asJsonString(studentTest))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        // I guess this means something was added
         assertEquals(200, response.getStatus());
 
-        Student result = fromJsonString(response.getContentAsString(), Student.class);
-        assertNotEquals(0, result.getStudent_id());
+        int newStudentsID = fromJsonString(response.getContentAsString(), Integer.class);
+        assertEquals(4, newStudentsID);
 
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .get("/student/list")
+                                .get("/student")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        // verify that returned data contains the added course
-        Student[] student_list = fromJsonString(response.getContentAsString(), Student[].class);
+        // verify that returned data contains the added student
+        StudentDTO[] student_list = fromJsonString(response.getContentAsString(), StudentDTO[].class);
 
         boolean found = false;
-        for (Student s : student_list) {
-            if (s.getEmail().equals("ironmaiden@rock.com")) {
+        for (StudentDTO s : student_list) {
+            if (s.email().equals("ironmaiden@rock.com")) {
                 found = true;
             }
         }
@@ -108,7 +106,7 @@ public class JunitTestStudent {
         // Check case where same email is entered
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .post("/student/add")
+                                .post("/student")
                                 .content(asJsonString(studentTest))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
@@ -120,23 +118,28 @@ public class JunitTestStudent {
 
     @Test
     @DirtiesContext
-    public void deleteStudent() throws Exception {
+    public void deleteStudentWithEnrollment() throws Exception {
         MockHttpServletResponse response;
 
         // Test deleting student with existing enrollment
         response = mvc.perform(
-                MockMvcRequestBuilders
-                        .delete("/student/delete?id=1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                        MockMvcRequestBuilders
+                                .delete("/student/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
         assertEquals(400, response.getStatus());
+    }
 
-        // Test force delete on student with enrollment
+    @Test
+    @DirtiesContext
+    public void forceDeleteStudent() throws Exception {
+        MockHttpServletResponse response;
+
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .delete("/student/delete?id=1&FORCE=true")
+                                .delete("/student/1/?FORCE=true")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
@@ -146,36 +149,81 @@ public class JunitTestStudent {
         // Verify student no longer exists
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .get("/student/list")
+                                .get("/student")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        Student[] students = fromJsonString(response.getContentAsString(), Student[].class);
+        StudentDTO[] students = fromJsonString(response.getContentAsString(), StudentDTO[].class);
         boolean found = false;
-        for( Student s : students) {
-            if(s.getStudent_id() == 1) {
+        for( StudentDTO s : students) {
+            if(s.studentId() == 1) {
                 found = true;
             }
         }
         assertFalse(found);
+    }
 
-        // Test delete on student with no enrollment
+    @Test
+    @DirtiesContext
+    public void deleteStudentWithNoEnrollment() throws Exception{
+        MockHttpServletResponse response;
+
+        Student studentTest = new Student();
+        studentTest.setName("Bruce");
+        studentTest.setEmail("ironmaiden@rock.com");
+
+        // Simulates a successful response for adding a student
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .delete("/student/delete?id=4")
+                                .post("/student")
+                                .content(asJsonString(studentTest))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
         assertEquals(200, response.getStatus());
 
-        for( Student s : students) {
-            if(s.getStudent_id() == 1) {
+        // Test delete on student with no enrollment
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .delete("/student/4")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/student")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        StudentDTO[] students = fromJsonString(response.getContentAsString(), StudentDTO[].class);
+        boolean found = false;
+        for(StudentDTO s : students) {
+            if(s.studentId() == 4) {
                 found = true;
             }
         }
         assertFalse(found);
+    }
+
+    @Test
+    @DirtiesContext
+    public void deleteNonExistingStudent() throws Exception{
+        MockHttpServletResponse response;
+
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .delete("/student/5")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
     }
 
     @Test
@@ -187,16 +235,19 @@ public class JunitTestStudent {
 
         response = mvc.perform(
                         MockMvcRequestBuilders
-                                .get("/student/list")
+                                .get("/student")
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        Student[] students = fromJsonString(response.getContentAsString(), Student[].class);
+        System.out.println(response.getContentType());
+        //List<StudentDTO> students = new ArrayList<>();
+        StudentDTO[] students;
+        students = fromJsonString(response.getContentAsString(), StudentDTO[].class);
 
         assertEquals(200, response.getStatus());
 
         for(int i = 0; i < students.length; i++) {
-            assertEquals(students[i].getEmail(), (emails[i]));
+            assertEquals(students[i].email(), (emails[i]));
         }
 
     }
