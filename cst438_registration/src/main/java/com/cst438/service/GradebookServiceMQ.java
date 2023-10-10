cacc005f735d763974056e77f66005f11c04f9a6
OsCortes88 <ocortestinoco@csumb.edu>
2023-10-10 00:12:23 -0700

    Integrated REST backends using messages

--- cst438_registration/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_registration/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,15 +25,23 @@ public class GradebookServiceMQ implements GradebookService {
 	
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
-	
+
 	Queue gradebookQueue = new Queue("gradebook-queue", true);
 
+	@Bean
+	public Queue createQueue() {
+		return new Queue("registration-queue", true);
+	}
 	// send message to grade book service about new student enrollment in course
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
-		System.out.println("Start Message "+ student_email +" " + course_id); 
-		// create EnrollmentDTO, convert to JSON string and send to gradebookQueue
-		// TODO
+		System.out.println("Start Message "+ student_email +" " + course_id);
+
+		Enrollment en = enrollmentRepository.findByEmailAndCourseId(student_email, course_id);
+		EnrollmentDTO e = new EnrollmentDTO(en.getEnrollment_id(), student_email, student_name, course_id);
+		String data = asJsonString(e);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), data);
+		System.out.println("Message sent");
 	}
 	
 	@RabbitListener(queues = "registration-queue")
@@ -45,8 +54,13 @@ public class GradebookServiceMQ implements GradebookService {
 		 */
 		
 		// deserialize the string message to FinalGradeDTO[] 
-		
-		// TODO
+
+		FinalGradeDTO[] grades = fromJsonString(message, FinalGradeDTO[].class);
+		for (int i = 0; i < grades.length; i++) {
+			Enrollment studentEnrollment = enrollmentRepository.findByEmailAndCourseId(grades[i].studentEmail(), grades[i].courseId());
+			studentEnrollment.setCourseGrade(grades[i].grade());
+			enrollmentRepository.save(studentEnrollment);
+		}
 
 	}
 	
